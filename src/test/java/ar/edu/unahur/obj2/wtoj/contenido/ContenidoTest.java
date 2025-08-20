35e68a5c1239ce58f27b59bc6adeefd7e0fa3853
celesteFernandez <celestefernandezmansilla@gmail.com>
2025-08-20 18:13:24 -0300

    cambios

---
+++ src/test/java/ar/edu/unahur/obj2/wtoj/contenido/ContenidoTest.java
@@ -0,0 +1,25 @@
+package ar.edu.unahur.obj2.wtoj.contenido;
+
+import org.junit.jupiter.api.Test;
+
+public class ContenidoTest {
+    @Test
+    void testCosto() {
+        
+    }
+
+    @Test
+    void testGetTitulo() {
+
+    }
+
+    @Test
+    void testSetCostoBase() {
+
+    }
+
+    @Test
+    void testSetTitulo() {
+
+    }
+}
